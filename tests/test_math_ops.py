48716c9242347cd333f51ff8a1a3e391e9a62c3c
davidtsukiyama <davidtsukiyama@davidtsyamasMBP.attlocal.net>
2024-11-19 19:37:21 -0800

    reformatted files

--- tests/test_math_ops.py
+++ tests/test_math_ops.py
@@ -1,9 +1,11 @@
 import pytest
 from utils.math_ops import add, multiply
 
+
 def test_add():
     assert add(2, 3) == 5
     assert add([1, 2], [3, 4]) == [4, 6]  # Supports lists via NumPy or pandas
+
 
 def test_multiply():
     assert multiply(2, 3) == 6
