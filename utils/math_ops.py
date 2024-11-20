093b507d462a84c85e33b0a5eebc5cc5a09478bb
davidtsukiyama <davidtsukiyama@davidtsyamasMBP.attlocal.net>
2024-11-19 19:25:12 -0800

    first commit

---
+++ utils/math_ops.py
@@ -0,0 +1,12 @@
+def add(x, y):
+    """
+    Adds two numbers or lists element-wise.
+    """
+    return x + y
+
+
+def multiply(x, y):
+    """
+    Multiplies two numbers or lists element-wise.
+    """
+    return x * y
